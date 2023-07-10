829531e6fd5a689f615f82b33d05ed83b0c1b8cc
henrik242 <henrik242@users.noreply.github.com>
2023-07-10 14:25:43 +0200

    page counter

--- mørk-skog.js
+++ mørk-skog.js
@@ -27,11 +27,11 @@
             $(".byline, .user_settings_text, .editing_bar, .thread_excerpt, #user_unread_count, .login_count, .activity_item_block, .sidebar, .shout_actions, div, .count").css({
                 "color": "lightgray"
             });
-            $(".dropdown_box, .dropdown_container, .annotation, #menu, .submenu, .tabs").css({
+            $(".dropdown_box, .dropdown_container, .annotation, #menu, .submenu, .tabs, .page_selector a").css({
                 "background": "#222",
                 "border-color": "darkgray"
             });
-            $(".sale_type, .tabs_tab.selected").css({
+            $(".sale_type, .tabs_tab.selected, .current_page").css({
                 "background": "gray",
             });
             $(".panel_hide, .panel_move_up, .panel_move_down").css({
