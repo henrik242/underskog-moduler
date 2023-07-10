718f37b5f4fbc278fdfb198e551abe963070b347
henrik242 <henrik242@users.noreply.github.com>
2023-07-10 12:43:29 +0200

    Litt kjappere temasvitsj

--- mørk-skog.js
+++ mørk-skog.js
@@ -1,27 +1,23 @@
 (function ($) {
 
-// MØRK SKOG™
-// Foreslå endringer på https://github.com/henrik242/mork-skog
+    // MØRK SKOG™
+    // Foreslå endringer på https://github.com/henrik242/mork-skog
 
-    if (!autoMørke || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        // dark mode
-
-        Underskog.onPageModification(function () {
+    function mørke() {
+        if (!autoMørke || (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            // dark mode
 
             $('body').css({
                 'background': 'black',
                 'color': 'white'
             });
-
             $("body a").css({
                 'color': 'lightgray'
             });
-
             $('.body').css({
                 'background': '#111',
                 'color': 'white'
             });
-
             $('.post').css({
                 'background': 'black',
                 'color': 'white'
@@ -29,15 +25,17 @@
             $('h2').css({
                 'color': 'white'
             });
-
             $(".tabbed_menu").css({
                 "background": "black"
             });
-
             $(".user_settings>li .user_settings_text").css({
                 "color": "lightgrey"
             });
-
-        });
+        }
     }
+
+    mørke();
+    Underskog.onPageModification(function () {
+        mørke();
+    });
 })(jQuery);
