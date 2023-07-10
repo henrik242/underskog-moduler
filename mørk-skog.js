2fbd3cb9414860617cca7a9c252371a2ce0d3fea
henrik242 <henrik242@users.noreply.github.com>
2023-07-10 14:21:39 +0200

    tabs og highlights

--- mørk-skog.js
+++ mørk-skog.js
@@ -27,15 +27,18 @@
             $(".byline, .user_settings_text, .editing_bar, .thread_excerpt, #user_unread_count, .login_count, .activity_item_block, .sidebar, .shout_actions, div, .count").css({
                 "color": "lightgray"
             });
-            $(".dropdown_box, .dropdown_container, .annotation, #menu, .submenu").css({
+            $(".dropdown_box, .dropdown_container, .annotation, #menu, .submenu, .tabs").css({
                 "background": "#222",
                 "border-color": "darkgray"
             });
-            $(".sale_type").css({
+            $(".sale_type, .tabs_tab.selected").css({
                 "background": "gray",
             });
             $(".panel_hide, .panel_move_up, .panel_move_down").css({
                 "opacity": "1.0",
+            });
+            $(".highlight").css({
+                "color": "black",
             });
         }
     }
