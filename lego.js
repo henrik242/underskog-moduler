5c0d373e1c6d75c81aba11e901dc4fc4b5fb9aa2
henrik242 <henrik242@users.noreply.github.com>
2023-11-17 09:29:14 +0100

    be nice

--- lego.js
+++ lego.js
@@ -4,9 +4,11 @@ var legoStuff = 0;
 
   Underskog.onPageModification(function () {
     if (legoStuff < 3) {
-      $(".comment  > div.body > div, div.bulletin").each(function() {
-        legoStuff++; 
-        this.innerHTML = this.innerHTML.replace(/\b(ego)/ig, "L$&");
+      $(".comment  > div.body > div, .bulletin > div > h2 > .tittel, .bulletin > div > .body > div").each(function() {
+        legoStuff++;
+        if (this.innerHTML.match(/\bego/i)) {
+          this.innerHTML = this.innerHTML.replace(/\b(ego)/ig, "L$&");
+        }
       });
     }
   });
